4d910d7c9eeda069b35a6fcb73f97f912f4147ed
Garfie Chiu <garfiechiu168@gmail.com>
2017-11-01 03:09:36 -0500

    Refactor player class

--- src/Java/com/garfiec/battleship/game/player/Local_Player.java
+++ src/Java/com/garfiec/battleship/game/player/Local_Player.java
@@ -1,4 +1,13 @@
-package Java.com.garfiec.battleship.game.board.player;
+package Java.com.garfiec.battleship.game.player;
 
-public class Local_Player implements Player {
+public class Local_Player {
+    protected boolean isLocal;
+
+    public Local_Player() {
+        isLocal = true;
+    }
+
+    public void makeMove(byte x, byte y) {
+
+    }
 }
--- src/Java/com/garfiec/battleship/game/player/Remote_Player.java
+++ src/Java/com/garfiec/battleship/game/player/Remote_Player.java
@@ -1,4 +1,14 @@
-package Java.com.garfiec.battleship.game.board.player;
+package Java.com.garfiec.battleship.game.player;
 
-public class Remote_Player implements Player {
+public class Remote_Player extends Local_Player {
+
+    public Remote_Player() {
+        super();
+        isLocal = false;
+    }
+
+    @Override
+    public void makeMove(byte x, byte y) {
+
+    }
 }
